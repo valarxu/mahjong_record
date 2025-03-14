176480ca18531b60b96a7665f62f55216dd32035
valarxu <7781862@qq.com>
2025-03-14 23:01:43 +0800

    update: 调整欢迎语

--- miniprogram/pages/chatBot/chatBot.js
+++ miniprogram/pages/chatBot/chatBot.js
@@ -16,7 +16,7 @@ Page({
       modelProvider: "deepseek", // 大模型服务厂商
       quickResponseModel: "deepseek-v3", // 快速响应模型 （混元 turbo, gpt4 turbo版，deepseek v3等）
       logo: "", // model 头像
-      welcomeMsg: "欢迎语", // model 欢迎语
+      welcomeMsg: "阿吉欢迎你", // model 欢迎语
     },
   },
   /**
